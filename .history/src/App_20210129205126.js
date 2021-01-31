764d3dde0bdd40a68087ca2f4c23b6e199f94be9
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-01-30 17:43:04 -0800

    editBookmark & bookmarkForm

---
+++ .history/src/App_20210129205126.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import AddBookmark from './addBookmark/addBookmark';
+import BookmarkApp from './bookmarkApp/bookmarkApp';
+
+
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import AddBookmark from './AddBookmark/AddBookmark';
+import EditBookmark from './EditBookmark/EditBookmark';
+import BookmarkList from './bookmarkList/BookmarkList';
+import BookmarksContext from './BookmarksContext';
+import Nav from './Nav/Nav';
+import config from './config';
+import './App.css';
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer AIzaSyBihMqr9ktofr1j1JcDwqO-Sg2s89FO6oA",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+  }
+
+  setShowAddForm(show) {
+    this.setState({
+      showAddForm: show
+    });
+  }
+
+  addBookmark(bookmark) {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark],
+      showAddForm: false
+    });
+  }
+
+  render() {
+    const page = this.state.showAddForm
+          ? <AddBookmark 
+                 showForm={show => this.setShowAddForm(show)} 
+                 handleAdd={bookmark => this.addBookmark(bookmark)}/>
+          : <BookmarkApp bookmarks={this.state.bookmarks} showForm={show => this.setShowAddForm(show)}/>; 
+
+    return (
+      <div className="App">
+        { page }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ .history/src/App_20210130171725.js
@@ -0,0 +1,174 @@
+
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import AddBookmark from './AddBookmark/AddBookmark';
+import EditBookmark from './EditBookmark/EditBookmark';
+import BookmarkList from './BookmarkList/BookmarkList';
+import BookmarksContext from './BookmarksContext';
+import Nav from './Nav/Nav';
+import config from './config';
+import './App.css';
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       bookmarks: [],
+//       showAddForm: false
+//     };
+//   }
+
+//   componentDidMount() {
+//     const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+//     const options = {
+//       method: 'GET',
+//       headers: {
+//         "Authorization": "Bearer AIzaSyBihMqr9ktofr1j1JcDwqO-Sg2s89FO6oA",
+//         "Content-Type": "application/json"
+//       }
+//     };
+
+//     fetch(url, options)
+//       .then(res => {
+//         if(!res.ok) {
+//           throw new Error('Something went wrong, please try again later.');
+//         }
+//         return res;
+//       })
+//       .then(res => res.json())
+//       .then(data => {
+//         this.setState({
+//           bookmarks: data,
+//           error: null
+//         });
+//       })
+//       .catch(err => {
+//         this.setState({
+//           error: err.message
+//         });
+//       });
+
+//   }
+
+//   setShowAddForm(show) {
+//     this.setState({
+//       showAddForm: show
+//     });
+//   }
+
+//   addBookmark(bookmark) {
+//     this.setState({
+//       bookmarks: [...this.state.bookmarks, bookmark],
+//       showAddForm: false
+//     });
+//   }
+
+//   render() {
+//     const page = this.state.showAddForm
+//           ? <AddBookmark 
+//                  showForm={show => this.setShowAddForm(show)} 
+//                  handleAdd={bookmark => this.addBookmark(bookmark)}/>
+//           : <BookmarkApp bookmarks={this.state.bookmarks} showForm={show => this.setShowAddForm(show)}/>; 
+
+//     return (
+//       <div className="App">
+//         { page }
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+class App extends Component {
+  state = {
+    bookmarks: [],
+    error: null,
+  };
+
+  setBookmarks = bookmarks => {
+    this.setState({
+      bookmarks,
+      error: null,
+    })
+  }
+
+  addBookmark = bookmark => {
+    this.setState({
+      bookmarks: [ ...this.state.bookmarks, bookmark ],
+    })
+  }
+
+  deleteBookmark = bookmarkId => {
+    const newBookmarks = this.state.bookmarks.filter(bm =>
+      bm.id !== bookmarkId
+    )
+    this.setState({
+      bookmarks: newBookmarks
+    })
+  }
+
+  componentDidMount() {
+    fetch(config.API_ENDPOINT, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': "Bearer AIzaSyBihMqr9ktofr1j1JcDwqO-Sg2s89FO6oA"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          return res.json().then(error => Promise.reject(error))
+        }
+        return res.json()
+      })
+      .then(this.setBookmarks)
+      .catch(error => {
+        console.error(error)
+        this.setState({ error })
+      })
+  }
+
+  updateBookmark = updatedBookmark => {
+    this.setState({
+      bookmarks: this.state.bookmarks.map(bm =>
+        (bm.id !== updatedBookmark.id) ? bm : updatedBookmark
+      )
+    })
+  }
+
+  render() {
+    const contextValue = {
+      bookmarks: this.state.bookmarks,
+      addBookmark: this.addBookmark,
+      deleteBookmark: this.deleteBookmark,
+      updateBookmark: this.updateBookmark,
+    }
+    return (
+      <main className='App'>
+        <h1>Bookmarks!</h1>
+        <BookmarksContext.Provider value={contextValue}>
+          <Nav />
+          <div className='content' aria-live='polite'>
+            <Route
+              exact
+              path='/'
+              component={BookmarkList}
+            />
+            <Route
+              path='/add-bookmark'
+              component={AddBookmark}
+            />
+            <Route
+              path='/edit/:bookmarkId'
+              component={EditBookmark}
+            />
+          </div>
+        </BookmarksContext.Provider>
+      </main>
+    );
+  }
+}
+
+export default App;
